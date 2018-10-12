b6b329b8fd83bfcf65368dcd983fb727c2786580
miscbits <willow6786@gmail.com>
2018-10-11 21:11:05 -0400

    added github provider

--- config/services.php
+++ config/services.php
@@ -48,4 +48,10 @@ return [
         'redirect' => env('TWITTER_URL'),
     ],
 
+    'github' => [
+        'client_id' => env('GITHUB_ID'),
+        'client_secret' => env('GITHUB_SECRET'),
+        'redirect' => env('GITHUB_URL'),
+    ],
+
 ];
